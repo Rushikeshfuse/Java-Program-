c09017395c4f529b4948ba85d557d255b665a7bc
Rushikeshfuse <88372078+Rushikeshfuse@users.noreply.github.com>
2022-09-28 08:42:18 +0530

    Add files via upload

---
+++ MulFloat.java
@@ -0,0 +1,20 @@
+/* multiplication of float type value*/
+
+
+import java.util.*;
+
+class MulFloat
+{
+public static void main(String args[])
+{
+float a,b,c;
+Scanner scan=new Scanner(System.in);
+System.out.println("Enter the first no");
+a=scan.nextFloat();
+
+System.out.println("Enter the Second no");
+b=scan.nextFloat();
+
+c=a*b;
+System.out.println(c);
+}}
