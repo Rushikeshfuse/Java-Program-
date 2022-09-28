cc545c2739ec73dfac8e57231c538a46f7dc0870
Rushikeshfuse <88372078+Rushikeshfuse@users.noreply.github.com>
2022-09-28 08:36:43 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+class Hello
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+
+System.out.println("Enter a number");
+int a;
+a=scan.nextInt();
+System.out.println("-----------------");
+
+System.out.println("Enterd the number:-"+a);
+}}
